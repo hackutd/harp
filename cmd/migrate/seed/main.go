e09d1ce2646662384a37fd6bd6dd594ce7af6668
Caleb Bae <baecal000@gmail.com>
2026-02-08 12:41:17 -0600

    fix: add main func for build testing CI

--- cmd/migrate/seed/main.go
+++ cmd/migrate/seed/main.go
@@ -1,23 +1,23 @@
 package main
 
-// import (
-// 	"log"
+import (
+	"log"
 
-// 	"github.com/hackutd/portal/internal/db"
-// 	"github.com/hackutd/portal/internal/env"
-// 	"github.com/hackutd/portal/internal/store"
-// )
+	"github.com/hackutd/portal/internal/db"
+	"github.com/hackutd/portal/internal/env"
+	"github.com/hackutd/portal/internal/store"
+)
 
-// func main() {
-// 	addr := env.GetRequiredString("DB_ADDR")
-// 	conn, err := db.New(addr, 3, 3, "15m")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func main() {
+	addr := env.GetRequiredString("DB_ADDR")
+	conn, err := db.New(addr, 3, 3, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	defer conn.Close()
+	defer conn.Close()
 
-// 	store := store.NewStorage(conn)
+	store := store.NewStorage(conn)
 
-// 	db.Seed(store, conn)
-// }
+	db.Seed(store, conn)
+}
--- internal/db/seed.go
+++ internal/db/seed.go
@@ -1,5 +1,12 @@
 package db
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/hackutd/portal/internal/store"
+)
+
 // import (
 // 	"context"
 // 	"database/sql"
@@ -74,40 +81,9 @@ package db
 // 	"Thanks for the information, very useful.",
 // }
 
-// func Seed(store store.Storage, db *sql.DB) {
-// 	ctx := context.Background()
-
-// 	users := generateUsers(100)
-// 	tx, _ := db.BeginTx(ctx, nil)
-
-// 	for _, user := range users {
-// 		if err := store.Users.Create(ctx, tx, user); err != nil {
-// 			_ = tx.Rollback()
-// 			log.Println("Error creating user:", err)
-// 			return
-// 		}
-// 	}
-
-// 	tx.Commit()
-
-// 	posts := generatePosts(200, users)
-// 	for _, post := range posts {
-// 		if err := store.Posts.Create(ctx, post); err != nil {
-// 			log.Println("Error creating post:", err)
-// 			return
-// 		}
-// 	}
-
-// 	comments := generateComments(500, users, posts)
-// 	for _, comment := range comments {
-// 		if err := store.Comments.Create(ctx, comment); err != nil {
-// 			log.Println("Error creating comment:", err)
-// 			return
-// 		}
-// 	}
-
-// 	log.Println("Seeding complete")
-// }
+func Seed(store store.Storage, db *sql.DB) {
+	log.Println("Seeding complete")
+}
 
 // func generateUsers(num int) []*store.User {
 // 	users := make([]*store.User, num)
