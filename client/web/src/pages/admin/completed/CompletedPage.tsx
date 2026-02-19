86584160d6582f8a37809f1d8b11eacb4a8637ff
axv2655 <arnav.vedula@yahoo.com>
2026-02-19 00:16:09 -0600

    fix: Dealt with issues in the front end and backend audits

--- client/web/src/pages/admin/completed/CompletedPage.tsx
+++ client/web/src/pages/admin/completed/CompletedPage.tsx
@@ -188,6 +188,7 @@ export default function CompletedPage() {
                       application={applicationDetail}
                       selectedReview={selectedReview}
                       isExpanded={false}
+                      onAiPercentageUpdate={() => {}}
                     />
 
                     {/* Reviewer notes section */}
