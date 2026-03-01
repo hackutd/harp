b756f04e4c9f58c8c690abe61615c791b1422805
Caleb Bae <baecal000@gmail.com>
2026-03-01 12:40:59 -0600

    chore: clean and remove uneeded FE code

---
+++ client/web/src/pages/superadmin/reviews/ReviewsPage.tsx
@@ -0,0 +1,8 @@
+export default function ReviewsPage() {
+  return (
+    <div>
+      Future super admin reviews management as well as where the super admins
+      (codirectors) will accept/reject/waitlist all submitted apps
+    </div>
+  );
+}
